7619f71a470c9b122872767f3e2e9e49a7a9b8f5
jouvash <JouVash@MacBook-Pro-de-Jose.local>
2015-05-12 08:58:37 -0430

    listo funciones subImagen y hsplit/vsplit

--- Proyecto 1/src/Imagen.hs
+++ Proyecto 1/src/Imagen.hs
@@ -5,7 +5,7 @@ module Imagen
   where
 
 import Graphics.Mosaico.Imagen (Color(Color, rojo, verde, azul), Imagen(Imagen, altura, anchura, datos))
-
+import Data.List (genericDrop, genericTake, genericIndex)
 
 
 subImagen
@@ -13,15 +13,18 @@ subImagen
   -> Integer -> Integer
   -> Imagen -> Imagen
 
-subImagen xInicial yInicial anchura' altura' (Imagen anchura'' altura'' pixels) = Imagen anchura' altura' [ take anchura' (drop xInicial (color!!x)) | x <- [yInicial..altura'] ]
+subImagen xInicial yInicial anchura' altura' (Imagen _ _ pixels) = 
+    Imagen anchura' altura' [ genericTake anchura' (genericDrop xInicial (genericIndex pixels x)) | x <- [yInicial..altura'] ]
 
 
 
 hSplit :: Imagen -> (Imagen, Imagen)
-hSplit = undefined
+hSplit (Imagen anchura' altura' pixels) = (subImagen 0 (quot altura' 2) anchura' (quot altura' 2) (Imagen anchura' altura' pixels), 
+        subImagen 0 0 anchura' (quot altura' 2) (Imagen anchura' altura' pixels))
 
 vSplit :: Imagen -> (Imagen, Imagen)
-vSplit = undefined
+vSplit (Imagen anchura' altura' pixels) = (subImagen 0 0 (quot anchura' 2) altura' (Imagen anchura' altura' pixels), 
+        subImagen (quot anchura' 2) 0 (quot anchura' 2) altura' (Imagen anchura' altura' pixels))
 
 colorPromedio :: Imagen -> Color
 colorPromedio = undefined
