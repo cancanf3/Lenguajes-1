568bedffa5a53a4a6ecb98cecb6a5fdfc125c34e
jouvash <JouVash@MacBook-Pro-de-Jose.local>
2015-05-12 23:16:31 -0430

    listo color promedio

--- Proyecto 1/src/Imagen.hs
+++ Proyecto 1/src/Imagen.hs
@@ -5,7 +5,7 @@ module Imagen
   where
 
 import Graphics.Mosaico.Imagen (Color(Color, rojo, verde, azul), Imagen(Imagen, altura, anchura, datos))
-import Data.List (genericDrop, genericTake, genericIndex)
+import Data.List (genericDrop, genericTake, genericIndex, genericLength)
 
 
 subImagen
@@ -27,4 +27,16 @@ vSplit (Imagen anchura' altura' pixels) = (subImagen 0 0 (quot anchura' 2) altur
         subImagen (quot anchura' 2) 0 (quot anchura' 2) altura' (Imagen anchura' altura' pixels))
 
 colorPromedio :: Imagen -> Color
-colorPromedio = undefined
+colorPromedio (Imagen anchura' altura' color) = Color {rojo = fromInteger ((rojoSuma todo)`div`cantidad) , verde = fromInteger ((verdeSuma todo)`div`cantidad), azul = fromInteger ((azulSuma todo)`div`cantidad) }
+    where todo = [ genericIndex (genericIndex color y) x | y <- [0..(altura'-1)], x <- [0..(anchura'-1)] ]
+          cantidad = genericLength todo
+
+rojoSuma :: [Color] -> Integer
+rojoSuma []                 = 0
+rojoSuma (Color r _ _:xs)   = toInteger(r) + rojoSuma xs 
+verdeSuma :: [Color] -> Integer
+verdeSuma []                 = 0
+verdeSuma ((Color _ v _):xs) = toInteger(v) + verdeSuma xs 
+azulSuma :: [Color]  -> Integer
+azulSuma []                  = 0
+azulSuma ((Color _ _ a):xs)  = toInteger(a) + azulSuma xs 
