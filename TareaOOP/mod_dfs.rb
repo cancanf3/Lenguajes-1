4bdc0156b28b39a773ea84bf0c047e587fddac80
jouvash <JouVash@MacBook-Pro-de-Jose.local>
2015-06-07 10:53:40 -0430

    Agregando comentarios a dfs y fold

--- TareaOOP/mod_dfs.rb
+++ TareaOOP/mod_dfs.rb
@@ -1,3 +1,20 @@
+=begin
+
+Mixin:              Permite obtener la funcionalidad de recorridos Depth-first search.
+Requerimientos:     La clase debe tener implementado un iterador bajo el nombre de each.
+Metodos:            dfs : Realiza un recorrido depth-first search sobre la estructura,
+                          mientras permite realizar instrucciones con los nodos recorridos.
+                    dfs!: Realiza un recorrido depth-first search sobre la estructura, 
+                          mientras permite realizar instrucciones con los nodos recorridos
+                          y estas instrucciones pueden modificar a los mismos.
+Autores:            Jose Peña 11-10775 y Amin Arria 11-10053
+
+=end
+
+
+
+
+
 module DFS
     def dfs &block
       yield self
--- TareaOOP/mod_fold.rb
+++ TareaOOP/mod_fold.rb
@@ -1,3 +1,27 @@
+=begin
+
+mixin:              Permite obtener flexibilidad en el uso de operaciones a toda una estructura
+                    conectada. A través de un recorrido breadth-first search, los metodos pueden
+                    realizar operaciones sobre toda la estructura.
+Requerimientos:     La clase debe tener implementado un iterador bajo el nombre de each.
+Metodos:            fold:   Es un metodo que al ir iterando sobre la estructura va realizando
+                            operaciones binarias entre el valor del nodo y un acomulador, las 
+                            cuales se van acomulando en el acomulador.
+                    map:    Es un metodo que aplica una operacion sobre cada uno de los valores 
+                            del nodo que va recorriendo.
+                    map!:   Es un meotodo que aplica una operacion sobre cada uno de los valores
+                            del nodo que va recorriendo y almacena el resultado en el valor del 
+                            nodo.
+                    bfs:    Es el metodo que permite recorrer la estructura con un sentido 
+                            breadth-first search, todos los metodos de este mixin hacen uso de
+                            este procedimiento para recorrer sobre las estructuras.
+Autores:            Jose Peña 11-10775 y Amin Arria 11-10053
+
+
+=end 
+
+
+
 module Foldable
 
     def fold(b, &bloque)
