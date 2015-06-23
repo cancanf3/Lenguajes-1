78ebbfede5d13f4fc999f2e2fdedb4142dda0b0e
Amin Arria <arria.amin@gmail.com>
2015-06-23 14:20:27 -0430

    mejor

--- ProyectoProlog/proyecto2.pl
+++ ProyectoProlog/proyecto2.pl
@@ -1,6 +1,14 @@
 
 
 
+%% cosa(X,Y) :- between(0,9,X), between(0,9,Y).
+
+%% saltoEnPosicion(T, N, X, Y, S) :- 
+
+listaValida([]).
+listaValida([X|XS]) :- X >= 0, X =< 10, listaValida(XS).
 
 tableroValido([_|[]]).
-tableroValido([X,Y|XS]) :- length(X, N), length(Y, N), tableroValido([Y|XS]).
+tableroValido([X,Y|XS]) :- listaValida(X), length(X, N), length(Y, N), tableroValido([Y|XS]).
+
+
