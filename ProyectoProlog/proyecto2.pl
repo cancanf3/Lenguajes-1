151aa01f0065cbabdfef5dc048dc945f17a7ff5e
jouvash <JouVash@MacBook-Pro-de-Jose.local>
2015-06-24 18:20:49 -0430

    Comentado y enviado

--- ProyectoProlog/proyecto2.pl
+++ ProyectoProlog/proyecto2.pl
@@ -1,33 +1,22 @@
+% Proyecto de programacion en Prolog
+% Laboratorio de Lenguajes de Programacion
+% Autores: Jose Peña (11-10775) Amin Arria(11-10053)
 
 
-%% saltoEnPosicion([[1,4,5,8],[7,4,1,9],[2,5,5,6],[9,8,3,8]], 3, 0, 0, solucion([1,4], 0,0, [este])).
 
-/*cosa(0,0).
-cosa(X,Y) :- cosa(X2,Y), X is X2+1.
-cosa(X,Y) :- cosa(X,Y2), Y is Y2+1.
-cosa(X,Y) :- cosa(X2,Y2), X is X2+1, Y is Y2+1.
 
-crap(0).
-crap(X) :- between(0,2,X).
-*/
+% Predicado que unifica un valor ubicado en una posicion especifica en un tablero.
+obtenerValor(T,X,Y,V) :- obtener(T, X, FX), obtener(FX, Y, V).
+
+% Predicado auxiliar usado por obtenerValor.
 obtener([X|_], 0, X).
 obtener([_|XS], N, FX) :- N1 is N-1, obtener(XS, N1, FX).
 
-obtenerValor(T,X,Y,V) :- obtener(T, X, FX), obtener(FX, Y, V).
 
-aux(T, 0, X, Y, solucion([V], X, Y, [])) :- obtenerValor(T, X, Y, V),!.
+% Predicado que unifica una solucion a partir de un 
+% tablero, una cantidad de saltos y una ubicacion inicial.
 
-aux(T, N, X, Y, solucion([L|LS], X, Y, [norte|PS])) :- obtenerValor(T, X, Y, L), 
-         N1 is N-1, N1 >= 0, X1 is X-1, X1 >= 0, aux(T, N1, X1, Y, solucion(LS, X1, Y, PS)).
-aux(T, N, X, Y, solucion([L|LS], X, Y, [sur|PS])) :- obtenerValor(T, X, Y, L), 
-         N1 is N-1, N1 >= 0, X1 is X+1, aux(T, N1, X1, Y, solucion(LS, X1, Y, PS)).
-aux(T, N, X, Y, solucion([L|LS], X, Y, [este|PS])) :- obtenerValor(T, X, Y, L), 
-         N1 is N-1, N1 >= 0, Y1 is Y+1, aux(T, N1, X, Y1, solucion(LS, X, Y1, PS)).
-aux(T, N, X, Y, solucion([L|LS], X, Y, [oeste|PS])) :- obtenerValor(T, X, Y, L), 
-         N1 is N-1, N1 >= 0, Y1 is Y-1, Y1 >= 0, aux(T, N1, X, Y1, solucion(LS, X, Y1, PS)).
-
-
-saltoEnPosicion(T, 0, X, Y, solucion([V], X, Y, [])) :- obtenerValor(T, X, Y, V).
+saltoEnPosicion(T, 0, X, Y, solucion([V], X, Y, [])) :- obtenerValor(T, X, Y, V),!.
 
 saltoEnPosicion(T, N, X, Y, solucion([L|LS], X, Y, [norte|PS])) :- tableroValido(T), obtenerValor(T, X, Y, L), 
          N1 is N-1, X1 is X-1, X>=0, aux(T, N1, X1, Y, solucion(LS, X1, Y, PS)).
@@ -42,14 +31,31 @@ saltoEnPosicion(T, N, X, Y, solucion([L|LS], X, Y, [oeste|PS])) :- tableroValido
          N1 is N-1, N1 >= 0, Y1 is Y-1, Y1 >= 0, aux(T, N1, X, Y1, solucion(LS, X, Y1, PS)).
 
 
+% Predicado auxiliar de saltoEnPosicion que unifica una solucion a partir de un 
+% tablero, una cantidad de saltos y una ubicacion inicial.
+aux(T, 0, X, Y, solucion([V], X, Y, [])) :- obtenerValor(T, X, Y, V),!.
+
+aux(T, N, X, Y, solucion([L|LS], X, Y, [norte|PS])) :- obtenerValor(T, X, Y, L), 
+         N1 is N-1, N1 >= 0, X1 is X-1, X1 >= 0, aux(T, N1, X1, Y, solucion(LS, X1, Y, PS)).
+aux(T, N, X, Y, solucion([L|LS], X, Y, [sur|PS])) :- obtenerValor(T, X, Y, L), 
+         N1 is N-1, N1 >= 0, X1 is X+1, aux(T, N1, X1, Y, solucion(LS, X1, Y, PS)).
+aux(T, N, X, Y, solucion([L|LS], X, Y, [este|PS])) :- obtenerValor(T, X, Y, L), 
+         N1 is N-1, N1 >= 0, Y1 is Y+1, aux(T, N1, X, Y1, solucion(LS, X, Y1, PS)).
+aux(T, N, X, Y, solucion([L|LS], X, Y, [oeste|PS])) :- obtenerValor(T, X, Y, L), 
+         N1 is N-1, N1 >= 0, Y1 is Y-1, Y1 >= 0, aux(T, N1, X, Y1, solucion(LS, X, Y1, PS)).
 
 
+% Predicado que unifica una solucion a partir de un tablero,
+% calculando por backtracking todas las posiciones iniciales.
 
 salto([T|TS], N, S) :- length([T|TS],Xtam),length(T,Ytam),!, between(0, Xtam, X), between(0,Ytam,Y),  saltoEnPosicion([T|TS], N, X, Y, S).
 
+
+% Predicado que determina se las filas del tablero son validas.
 listaValida([]).
 listaValida([X|XS]) :- X >= 0, X =< 10, listaValida(XS).
 
+% Predicado que determina si un tablero dado es valido.
 tableroValido([_|[]]).
 tableroValido([X,Y|XS]) :- listaValida(X), length(X, N), length(Y, N), tableroValido([Y|XS]).
 
